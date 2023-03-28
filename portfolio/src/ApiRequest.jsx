dd3b0424d169a9f5357fcf919eef5b8c32ab4037
MADdantas <matheussdantas19@gmail.com>
2023-03-27 21:08:19 -0300

    Added humidity, wind speed and link to map

--- portfolio/src/ApiRequest.jsx
+++ portfolio/src/ApiRequest.jsx
@@ -1,6 +1,8 @@
 import axios from "axios"
 import { useState } from "react"
 import './ApiRequest.css'
+import humidity from './assets/humidity.jpg'
+import wind from './assets/wind-speed.png'
 
 const requestWeather = async (location) => {
     try{
@@ -9,13 +11,13 @@ const requestWeather = async (location) => {
         return data
     } catch(e) {
         console.log(e);
-        return e
+        return "Enter a place"
     }
 }
 
 function Weather() {
 
-    const [location, setLocation] = useState();
+    const [location, setLocation] = useState("Enter a place");
 
     async function handleButtonSubmmit() {
         const response = await requestWeather(document.getElementById("location").value);
@@ -49,18 +51,26 @@ function Weather() {
     return (
         <div className="Weather">
             <div>
-                <input type="text" id="location" size={40} placeholder="Type a city name, Zipcode or Postcode"></input>
+                <input type="text" id="location" className="search-box" placeholder="Type a city name, Zipcode or Postcode"></input>
                 <button onClick={handleButtonSubmmit}>
                     Search
                 </button>
             </div>
-            {location && (
+            {location != "Enter a place" ? (
                 <div>
-                    <h2 className="hltext" align="right"><img src="https://pngimg.com/uploads/gps/small/gps_PNG66.png" height="30"></img> {location?.data?.location?.name} - {location?.data?.location?.region} - {location?.data?.location?.country.toUpperCase()}</h2>
-                    <h1 className="hltext">{location?.data?.location?.localtime.substring(11,location?.data?.location?.localtime.length)}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src={location?.data?.current?.condition?.icon} alt="weather-image" className="icon"></img> {location?.data?.current?.temp_c} °C / {location?.data?.current?.temp_f} F</h1>
-                    <h4 className="hltext" align="right">UV index: {indiceUv(location?.data?.current?.uv)}</h4>
+                    <h2 align="right"><img src="https://pngimg.com/uploads/gps/small/gps_PNG66.png" height="30"></img> <a href={`https://www.google.com/maps/@${location?.data?.location?.lat},${location?.data?.location?.lon},12z`} target="_blank">{location?.data?.location?.name} - {location?.data?.location?.region} - {location?.data?.location?.country.toUpperCase()}</a></h2>
+                    <h1>{location?.data?.location?.localtime.substring(11,location?.data?.location?.localtime.length)}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src={location?.data?.current?.condition?.icon} alt="weather-image" className="icon"></img> {location?.data?.current?.temp_c} °C / {location?.data?.current?.temp_f} F</h1>
+                    <h4 align="right">UV index: {indiceUv(location?.data?.current?.uv)}</h4>
+                    <h2>
+                    <img src={humidity} height="40px" align="left" className="details"></img> 
+                    <div align="left" height="40px">Humidity {location?.data?.current?.humidity}%</div>
+                    </h2>
+                    <h2>
+                    <img src={wind} height="40px" align="left" className="details"></img>
+                    <div align="left">Wind Speed {location?.data?.current?.wind_kph} kph / {location?.data?.current?.wind_mph} mph</div>
+                    </h2>
                 </div>
-            )}
+            ) :("")}
         </div>
     )
 }
